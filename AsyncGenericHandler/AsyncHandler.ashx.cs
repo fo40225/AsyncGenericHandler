11d80403ea3dc7dbd009661c56fc7ff78f94dd6b
fo40225 <fo40225@hotmail.com>
2015-10-11 19:12:53 +0800

    first commit

---
+++ AsyncGenericHandler/AsyncHandler.ashx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace AsyncGenericHandler
+{
+    /// <summary>
+    /// AsyncHandler 的摘要描述
+    /// </summary>
+    public class AsyncHandler : HttpTaskAsyncHandler
+    {
+        public override async Task ProcessRequestAsync(HttpContext context)
+        {
+            await Task.Delay(0);
+        }
+    }
+}
